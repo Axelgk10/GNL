55c5b1be5cde2eb1a570e086edc1a9c4e9ec7e1c
Axelgk10 <axelkonert@gmail.com>
2025-05-20 11:29:19 +0200

    Create gnl_utils.c

---
+++ gnl_utils.c
@@ -0,0 +1,67 @@
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*res;
+
+	if (!s1 && !s2)
+		return (NULL);
+	res = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!res)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		res[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		res[i++] = s2[j];
+	res[i] = '\0';
+	free(s1);
+	return (res);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*p;
+	size_t			i;
+	size_t			n;
+
+	n = size * nmemb;
+	p = malloc(n);
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *) s);
+		s++;
+	}
+	if ((char)c == *s)
+		return ((char *)s);
+	return (NULL);
+}
